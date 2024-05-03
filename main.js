ca4365eccbac12da56f08c50f7427bd2b966fcb4
Νίκος Ζευγόλης <nzevgolisda@gmail.com>
2024-05-03 19:59:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 
-function addImage2Body(name, description, price) {
-    let item = new Item(name, description, price);
-    document.body.appendChild(item.element);
-}
+class Item {
+    constructor([name, description, price]) {
+        let el_descr = document.createElement('div');
+        el_descr.innerHTML = ' Description: ' + description;
 
-function addImages() {
-    let i;
-    let data = [
-        {id: 0, price: 0, description: "", name: "[Name]"},
-        {id: 1, price: 0, description: "", name: "[Name]"},
-        {id: 2, price: 0, description: "", name: "[Name]"},
-        {id: 3, price: 0, description: "", name: "[Name]"},
-        {id: 4, price: 0, description: "", name: "[Name]"},
-        {id: 5, price: 0, description: "", name: "[Name]"}
-    ];
-    for (i = 0; i < data.length+1; i++) {
-        addImage2Body(data[i].id, data[i].description, data[i].price);
+        let el_price = document.createElement('div');
+        el_price.innerHTML = ' Price: ' + price + ' €';
+    
+        let el_img = document.createElement('img');
+        el_img.src = './img/'+name+'.jpg';
+
+        this.element = document.createElement('div');
+        this.element.appendChild(el_descr);
+        this.element.appendChild(el_price);
+        this.element.appendChild(el_img);
     }
 }
 
-addImages();
+function addImage2Body([name, description, price]) {
+    let item = new Item([name, description, price]);
+    document.body.appendChild(item.element);
+}
+
+function addImages(data) {
+    for (let i = 0; i < data.length; i++) {
+        item = [data[i].id, data[i].description, data[i].price];
+        addImage2Body(item);
+    }
+}
+
+let data = [
+    {id: 0, price: 0, description: "", name: "[Name]"},
+    {id: 1, price: 0, description: "", name: "[Name]"},
+    {id: 2, price: 0, description: "", name: "[Name]"},
+    {id: 3, price: 0, description: "", name: "[Name]"},
+    {id: 4, price: 0, description: "", name: "[Name]"},
+    {id: 5, price: 0, description: "", name: "[Name]"}
+];
+addImages(data);
