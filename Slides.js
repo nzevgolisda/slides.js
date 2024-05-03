2cd59f7dde85928907d33a50eca45c83536bad04
Νίκος Ζευγόλης <nzevgolisda@gmail.com>
2024-05-03 19:03:52 +0300

    Update Slides.js

--- Slides.js
+++ Slides.js
@@ -1,14 +1,14 @@
 
 let i = 0;
-let n = 64;
+let n = 5;
 let images = [];
 let time = 3000;
-for (i = 0; i < n; i++) {
+for (i = 0; i < n+1; i++) {
     images.push('./img/'+i+'.jpg');
 }
 function changeImg() {
     document.slide.src = images[i];
-    if (i < images.length - 1) {
+    if (i < images.length-1) {
         i++;
     }else {
         i = 0;
